64dd8f129c12e1cdcc55100cae804e2d386bc828
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-09-23 20:24:29 +0900

    0923 update

---
+++ python/Problems/onboarding_in_python/#6.py
@@ -0,0 +1,8 @@
+#6 닉네임 설정
+
+forms = [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]
+names = []
+for form in forms:
+    names.append(form[1])
+    
+print(names)
