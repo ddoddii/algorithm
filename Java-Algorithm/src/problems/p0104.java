921c2ff22d071770c456cf78ef3c449cb029aea2
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-06 17:40:37 +0900

    1006 inflearn

--- Java-Algorithm/src/problems/p0104.java
+++ Java-Algorithm/src/problems/p0104.java
@@ -1,8 +1,35 @@
 package problems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 // 단어 뒤집기
 public class p0104 {
+    public static List<String> solution( String[] words){
+        List<String> answer = new ArrayList<>();
+        for (String word : words){
+            StringBuilder reversed = new StringBuilder(word);
+            answer.add(reversed.reverse().toString());
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        String[] words = new String[N];
+
+        for (int i = 0 ; i < N ; i++){
+            words[i] = sc.next();
+        }
+
+        List<String> answers = solution(words);
+        for (String answer : answers){
+            System.out.println(answer);
+        }
+
 
     }
 }
--- Java-Algorithm/src/problems/p0105.java
+++ Java-Algorithm/src/problems/p0105.java
@@ -1,4 +1,41 @@
 package problems;
 
+import java.util.List;
+import java.util.Scanner;
+
+// 특정 문자 뒤집기
+// toCharArray 이용
 public class p0105 {
+    public static String solution(String input){
+        char[] charArray = input.toCharArray();
+        int left = 0;
+        int right = charArray.length -1 ;
+        while (left < right) {
+            if (!Character.isAlphabetic(charArray[left])) {
+                left++;
+            }
+            else if (!Character.isAlphabetic(charArray[right])) {
+                right ++;
+            }
+            else {
+                char temp = charArray[left];
+                charArray[left] = charArray[right];
+                charArray[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(charArray);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        String answer = solution(input);
+        System.out.println(answer);
+
+    }
 }
