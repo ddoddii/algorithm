e131bbc43206a0ae38174fe826043747a90a3d95
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-12 16:04:22 +0900

    1012 leetcode

---
+++ Java-Algorithm/src/leetcode/p11.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+//11. Container with most water
+public class p11 {
+    //sol1. O(n^2)
+    public static int maxArea(int[] height) {
+        int maxAmount = Integer.MIN_VALUE;
+        for (int i = 0 ; i < height.length - 1 ; i++){
+            for (int j = 1 ; j < height.length ; j++) {
+                int amount = (j-i) * Math.min(height[i], height[j]);
+                if (amount > maxAmount) {
+                    maxAmount = amount;
+                }
+            }
+        }
+        return maxAmount;
+
+    }
+
+    //sol2. O(n)
+    public static int maxArea2(int[] height) {
+        int maxAmount = Integer.MIN_VALUE;
+        int i = 0;
+        int j = height.length - 1;
+        while (i < j) {
+            int amount = (j-i) * Math.min(height[i], height[j]);
+            if (amount > maxAmount){
+                maxAmount = amount;
+            }
+            if (height[i] < height[j]){
+                i++;
+            }
+            else {
+                j--;
+            }
+
+        }
+        return maxAmount;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea2(height));
+    }
+}
---
+++ Java-Algorithm/src/leetcode/p392.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+//392. Is Subsequence
+public class p392 {
+    public static boolean isSubsequence(String s, String t) {
+        if (s.isEmpty()) return true;
+        char[] charsT = t.toCharArray();
+        char[] charsS = s.toCharArray();
+        int i = 0;
+        int j = 0;
+        while ( j < charsS.length){
+            while ( i < charsT.length){
+                if (charsS[j] == charsT[i])  {
+                    i++;
+                    j++;
+                    if (j == charsS.length){
+                        return true;
+                    }
+                }
+                else {
+                    i++;
+                }
+            }
+
+            j++;
+
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "b";
+        String t = "abc";
+        System.out.println(isSubsequence(s,t));
+
+    }
+}
