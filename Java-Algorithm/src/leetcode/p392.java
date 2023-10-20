a1ddf119cf6712e99f25689874260e0c6831d02f
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-20 13:15:46 +0900

    1012 leetcode refactor

--- Java-Algorithm/src/leetcode/p392.java
+++ Java-Algorithm/src/leetcode/p392.java
@@ -2,37 +2,39 @@ package leetcode;
 
 //392. Is Subsequence
 public class p392 {
-    public static boolean isSubsequence(String s, String t) {
-        if (s.isEmpty()) return true;
-        char[] charsT = t.toCharArray();
-        char[] charsS = s.toCharArray();
-        int i = 0;
-        int j = 0;
-        while ( j < charsS.length){
-            while ( i < charsT.length){
-                if (charsS[j] == charsT[i])  {
-                    i++;
-                    j++;
-                    if (j == charsS.length){
-                        return true;
-                    }
-                }
-                else {
-                    i++;
-                }
-            }
 
-            j++;
-
+  public static boolean isSubsequence(String s, String t) {
+      if (s.isEmpty()) {
+          return true;
+      }
+    char[] charsT = t.toCharArray();
+    char[] charsS = s.toCharArray();
+    int i = 0;
+    int j = 0;
+    while (j < charsS.length) {
+      while (i < charsT.length) {
+        if (charsS[j] == charsT[i]) {
+          i++;
+          j++;
+          if (j == charsS.length) {
+            return true;
+          }
+        } else {
+          i++;
         }
-        return false;
+      }
+
+      j++;
 
     }
+    return false;
 
-    public static void main(String[] args) {
-        String s = "b";
-        String t = "abc";
-        System.out.println(isSubsequence(s,t));
+  }
 
-    }
+  public static void main(String[] args) {
+    String s = "b";
+    String t = "abc";
+    System.out.println(isSubsequence(s, t));
+
+  }
 }
