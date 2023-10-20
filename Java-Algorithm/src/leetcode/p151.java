522e77d7d7bbd09df652a92d51016986f3f5af23
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-20 13:15:29 +0900

    1009 leetcode refactor

--- Java-Algorithm/src/leetcode/p151.java
+++ Java-Algorithm/src/leetcode/p151.java
@@ -7,17 +7,18 @@ import java.util.List;
 
 // 151. Reverse word in string
 public class p151 {
-    public static String reverseWords(String s) {
-        s = s.trim();
-        String[] words = s.split("\\s+");
-        List<String> wordList = new ArrayList<>(Arrays.asList(words));
-        Collections.reverse(wordList);
-        return String.join(" ", wordList);
 
-    }
+  public static String reverseWords(String s) {
+    s = s.trim();
+    String[] words = s.split("\\s+");
+    List<String> wordList = new ArrayList<>(Arrays.asList(words));
+    Collections.reverse(wordList);
+    return String.join(" ", wordList);
 
-    public static void main(String[] args) {
-        String s = "   the sky    is blue";
-        System.out.println(reverseWords(s));
-    }
+  }
+
+  public static void main(String[] args) {
+    String s = "   the sky    is blue";
+    System.out.println(reverseWords(s));
+  }
 }
