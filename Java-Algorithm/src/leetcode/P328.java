6d89d2a1eb4d78b378a787ac05e4936ffe92316b
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-10-22 14:19:32 +0900

    1021 leetcode refactor

--- Java-Algorithm/src/leetcode/P328.java
+++ Java-Algorithm/src/leetcode/P328.java
@@ -46,21 +46,3 @@ public class P328 {
   }
 
 }
-
-class ListNode {
-
-  int val;
-  ListNode next;
-
-  ListNode() {
-  }
-
-  ListNode(int val) {
-    this.val = val;
-  }
-
-  ListNode(int val, ListNode next) {
-    this.val = val;
-    this.next = next;
-  }
-}
