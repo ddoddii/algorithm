3db64fec57d8c6784a4621d8b8db1ea6e64604fe
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-01 12:42:11 +0900

    1031 leetcode

--- Java-Algorithm/src/leetcode/P1161.java
+++ Java-Algorithm/src/leetcode/P1161.java
@@ -1,5 +1,49 @@
 package leetcode;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 // 1161. Maximum Level Sum of a Binary Tree
 public class P1161 {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        int sum;
+        int level = 0;
+        int maxSum = Integer.MIN_VALUE;
+        Map<Integer, Integer> levelSum = new HashMap<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            sum = 0;
+            level++;
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = q.poll();
+                sum += currNode.val;
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+            levelSum.put(level, sum);
+        }
+        return getMinlevelWithMaxSum(levelSum);
+
+    }
+
+    public int getMinlevelWithMaxSum(Map<Integer, Integer> levelSum) {
+        Map.Entry<Integer, Integer> maxEntry = levelSum.entrySet().stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .orElse(null);
+
+        return maxEntry.getKey();
+    }
+
+
 }
--- Java-Algorithm/src/leetcode/P700.java
+++ Java-Algorithm/src/leetcode/P700.java
@@ -2,4 +2,22 @@ package leetcode;
 // 700. Search in a Binary Search Tree
 
 public class P700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        return findNode(root, val);
+    }
+
+    public TreeNode findNode(TreeNode node, int val) {
+        if (node == null) {
+            return null;
+        } else if (node.val > val) {
+            return findNode(node.left, val);
+        } else if (node.val < val) {
+            return findNode(node.right, val);
+        } else {
+            return node;
+        }
+    }
 }
