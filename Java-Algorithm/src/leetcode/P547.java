adfe14bf97e1524848e278fa63272cf99b8dcb89
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-05 15:06:09 +0900

    1105 leetcode

---
+++ Java-Algorithm/src/leetcode/P547.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+//547. Number of Provinces
+public class P547 {
+    public int findCircleNum(int[][] isConnected) {
+        int nodeSize = isConnected.length;
+        boolean[] visited = new boolean[nodeSize];
+        int province = 0;
+        for (int i = 0 ; i < nodeSize ; i++){
+            if (!visited[i]){
+                dfs(isConnected, visited, i);
+                province++;
+            }
+
+        }
+        return province;
+
+    }
+
+    public void dfs(int[][] isConnected, boolean[] visited, int node){
+        visited[node] = true;
+        for (int neighbor = 0 ; neighbor < isConnected.length ; neighbor++){
+            if (isConnected[node][neighbor] == 1 && !visited[neighbor]){
+                dfs(isConnected, visited, neighbor);
+            }
+        }
+    }
+
+}
